b67f5e4d73ac5c92980d2d31a5b7d9b4f1446ac1
Alex Smith <ironhawk222@yahoo.com>
2013-07-28 12:59:28 -0700

    wip

--- map_monsters.js
+++ map_monsters.js
@@ -1,6 +1,6 @@
 var Skeleton = function(level) {
 	this.level = parseInt(level);
-	this.hp = Math.ceil( Math.random() * 3 + (2 * this.level * this.level) );
+	this.hp = Math.ceil( Math.random() * 3 + (2 * this.level) );
 }
 
 Skeleton.prototype.image = function() {
@@ -9,7 +9,7 @@ Skeleton.prototype.image = function() {
 
 var Spider = function(level) {
 	this.level = parseInt(level);
-	this.hp = Math.ceil( Math.random() * 3 + (3 * this.level * this.level) );
+	this.hp = Math.ceil( Math.random() * 3 + (3 * this.level) );
 }
 
 Spider.prototype.image = function() {
